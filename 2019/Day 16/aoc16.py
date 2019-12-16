8a7ffa73065fbda7b66c37215034c32dccd78206
eirikhoe <eirik150787@hotmail.com>
2019-12-16 22:20:49 +0100

    Added solution to Day 16

--- 2019/Day 16/aoc16.py
+++ 2019/Day 16/aoc16.py
@@ -4,37 +4,61 @@ import copy
 import time
 import os
 from colorama import init
-from math import ceil
+from math import ceil,floor
+
 init()
 data_folder = Path(__file__).parent.resolve()
+start_time = time.perf_counter()
 
+def fft(digits,repetitions,phases,use_offset=False):
+    if use_offset:
+        offset = int(("").join([str(d) for d in digits[:7]]))
+    else:
+        offset = 0
+    
+    digits = np.tile(digits, repetitions)
+    n = len(digits)
+    pattern = [0, 1, 0, -1]
+    m = len(pattern)
+    method_lim = 9 * ceil(np.sqrt(n + 1))
+    offset_lim = floor(n / 2)
+    if offset >= offset_lim:
+        index = np.arange(n - 1, offset-1, -1)
+    else:
+        first_index = np.arange(n - 1, offset_lim-1, -1)
+        second_index = np.arange(offset_lim-1, method_lim - 1, -1)
+    for k in np.arange(phases):
+        old_digits = copy.deepcopy(digits)
+        if offset >= offset_lim:
+            digits[index] = np.cumsum(old_digits[index], dtype=int)
+            digits[index] = np.mod(digits[index],10)
+        else:
+            digits[first_index] = np.cumsum(old_digits[first_index], dtype=int)
+            for i in second_index:
+                digits[i] = digits[i + 1]
+                lims = np.arange(i, n, i + 1)
+                for j, lim in enumerate(lims):
+                    if ((j + 1) % 4) < 2:
+                        digits[i] += np.sum(old_digits[lim : lim + j + 1])
+                    else:
+                        digits[i] -= np.sum(old_digits[lim : lim + j + 1])
+            
+            for i in np.arange(method_lim):
+                n_tiles = ceil((n + 1) / (m * (i + 1)))
+                coef = np.tile(np.repeat(pattern, i + 1), n_tiles)[1 : n + 1]
+                digits[i] = np.dot(coef, old_digits)
+            digits = np.mod(np.abs(digits), 10)
+    return "".join([str(d) for d in digits[offset : offset + 8]])
 
-
-
-
-def main(): 
+def main():
     file = data_folder / "day_16_input.txt"
     digits = np.array([int(d) for d in list(file.read_text())])
-    offset = ('').join([str(d) for d in digits[:7]])
-    orig_len = len(digits)
-    s = np.sum(digits)
-    digits = np.repeat(digits,10000)
-    n = len(digits)
-    print(n)
-    pattern = [0,1,0,-1]
-    m = len(pattern)
-    n_iterations = 100
-    for k in np.arange(n_iterations):
-        old_digits = copy.deepcopy(digits)
-        for i in np.arange(n):
-            lims = np.arange(i,i+1,n)
-            (q,r) = divmod(i+1,orig_len)
-            n_tiles = ceil((n+1)/(m*(i+1)))
-            coef = np.tile(np.repeat(pattern,i+1),n_tiles)[1:n+1]
-            
-            digits[i] = np.abs(np.dot(coef,old_digits)) % 10
-        print(f"{k} FFT iterations completed")
-    print(str(digits[offset:offset+8]))
+    
+    print('Part 1')
+    print(fft(digits,1,100,False))
+
+    print('Part 2')
+    print(fft(digits,10000,100,True))
 
 if __name__ == "__main__":
     main()
