ed2bb676d0f2f77a15e49c362a0aa938d04169bb
eirikhoe <eirik150787@hotmail.com>
2020-12-02 10:33:29 +0100

    Cleanup

--- 2020/Day 01/aoc1.py
+++ 2020/Day 01/aoc1.py
@@ -1,37 +1,39 @@
 from pathlib import Path
 import numpy as np
+
 data_folder = Path(".").resolve()
 
-def find_factors(entries,total,n_factors):
+
+def find_factors(entries, total, n_factors):
     if n_factors == 1:
         if total in entries:
             return [total]
         else:
             return None
 
-    for i,entry in enumerate(entries[:-1]):
-        factors = find_factors(entries[i+1:],total-entry,n_factors-1)
+    for i, entry in enumerate(entries[:-1]):
+        factors = find_factors(entries[i + 1 :], total - entry, n_factors - 1)
         if factors:
             factors.append(entry)
             return factors
     return None
-    
 
 
 def main():
     data = data_folder.joinpath("input.txt").read_text()
     data = [int(d) for d in data.split("\n")]
     total = 2020
-    
-    factors = find_factors(data,total,2)
+
+    factors = find_factors(data, total, 2)
     print("Part 1")
     print(f"The product of the two entries that sum to {total} is {np.prod(factors)}")
     print()
 
-    factors = find_factors(data,total,3)
+    factors = find_factors(data, total, 3)
     print("Part 2")
     print(f"The product of the three entries that sum to {total} is {np.prod(factors)}")
     print()
+
 
 if __name__ == "__main__":
     main()
--- 2020/Day 02/aoc2.py
+++ 2020/Day 02/aoc2.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-import numpy as np
 import re
+
 data_folder = Path(".").resolve()
 
 reg_password = re.compile(r"(\d+)-(\d+) ([a-z]): ([a-z]+)")
 
-def get_password_info(password_info):
+
+def get_password_components(password_info):
     match = reg_password.match(password_info)
     int_1 = int(match.groups()[0])
     int_2 = int(match.groups()[1])
@@ -13,16 +14,19 @@ def get_password_info(password_info):
     pwd = match.groups()[3]
     return int_1, int_2, char, pwd
 
+
 def is_password_valid_sled(password_info):
-    min_count, max_count, char, pwd = get_password_info(password_info)
+    min_count, max_count, char, pwd = get_password_components(password_info)
     char_count = pwd.count(char)
     return min_count <= char_count <= max_count
 
+
 def is_password_valid_toboggin(password_info):
-    index_1, index_2, char, pwd = get_password_info(password_info)
+    index_1, index_2, char, pwd = get_password_components(password_info)
     index_1 -= 1
     index_2 -= 1
-    return (pwd[index_1] == char) != (pwd[index_2] == char) 
+    return (pwd[index_1] == char) != (pwd[index_2] == char)
+
 
 def get_n_valid_passwords(passwords_info, policy_type):
     n_valid_passwords = 0
@@ -33,20 +37,21 @@ def get_n_valid_passwords(passwords_info, policy_type):
             n_valid_passwords += is_password_valid_toboggin(password_info)
         else:
             raise RuntimeError("Unknown policy type")
-    
+
     return n_valid_passwords
+
 
 def main():
     data = data_folder.joinpath("input.txt").read_text()
     data = data.split("\n")
-    
+
     print("Part 1")
-    n = get_n_valid_passwords(data,"sled")
+    n = get_n_valid_passwords(data, "sled")
     print(f"There are {n} valid passwords in the list when using sled policies")
     print()
 
     print("Part 2")
-    n = get_n_valid_passwords(data,"toboggin")
+    n = get_n_valid_passwords(data, "toboggin")
     print(f"There are {n} valid passwords in the list when using toboggin policies")
 
 
