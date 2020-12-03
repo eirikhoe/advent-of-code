571953d22f9ab8b122b9d4e7aad718d78564fe69
eirikhoe <eirik150787@hotmail.com>
2020-12-03 14:37:21 +0100

    Clean up day 3 2020

--- 2020/Day 03/aoc3.py
+++ 2020/Day 03/aoc3.py
@@ -1,32 +1,25 @@
 from pathlib import Path
 
 
-class map:
-    def __init__(self, data):
-        self.map = [list(line) for line in data.split("\n")]
-        self.dim = [len(self.map), len(self.map[0])]
-
-    def count_trees(self, slope):
-        x = 0
-        y = 0
-        n_trees = 0
-        while True:
-            x = (x + slope[1]) % self.dim[1]
-            y += slope[0]
-            if y >= self.dim[0]:
-                break
-            n_trees += self.map[y][x] == "#"
-        return n_trees
+def count_trees(tree_map, slope):
+    dim = [len(tree_map), len(tree_map[0])]
+    pos = [0, 0]
+    n_trees = 0
+    while pos[0] < dim[0]:
+        n_trees += tree_map[pos[0]][pos[1]] == "#"
+        pos[1] = (pos[1] + slope[1]) % dim[1]
+        pos[0] += slope[0]
+    return n_trees
 
 
 def main():
     data_folder = Path(".").resolve()
     data = data_folder.joinpath("input.txt").read_text()
+    tree_map = [list(line) for line in data.split("\n")]
 
     print("Part 1")
-    m = map(data)
     slope = (1, 3)
-    tree_count = m.count_trees(slope)
+    tree_count = count_trees(tree_map, slope)
     print(f"Following a slope of right {slope[1]} and down {slope[0]}")
     print(f"we encounter {tree_count} trees")
     print()
@@ -35,7 +28,7 @@ def main():
     rem_slopes = [(1, 1), (1, 5), (1, 7), (2, 1)]
     tree_product = tree_count
     for slope in rem_slopes:
-        tree_product *= m.count_trees(slope)
+        tree_product *= count_trees(tree_map, slope)
 
     print("If we multiply together the number of trees encountered on")
     print(f"each of the listed slopes we get {tree_product}")
