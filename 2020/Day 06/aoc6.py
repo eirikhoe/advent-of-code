2d799d11d2e893cd4bfc199a381e4da06467a3f2
eirikhoe <eirik150787@hotmail.com>
2020-12-06 09:55:42 +0100

    More cleanup day 6

--- 2020/Day 06/aoc6.py
+++ 2020/Day 06/aoc6.py
@@ -1,23 +1,12 @@
 from pathlib import Path
 
 
-def count_yes_anyone(group_answer):
+def count_yes_any(group_answer):
     return len(set.union(*group_answer))
 
 
-def count_yes_everyone(group_answer):
+def count_yes_all(group_answer):
     return len(set.intersection(*group_answer))
-
-
-count_yes = {"any": count_yes_anyone, "all": count_yes_everyone}
-
-
-def count_total(group_answers, count_type="any"):
-    total_yes = 0
-    for group_answer in group_answers:
-        total_yes += count_yes[count_type](group_answer)
-    return total_yes
-
 
 def main():
     data_folder = Path(".").resolve()
@@ -25,13 +14,15 @@ def main():
     group_answers = [[set(ans) for ans in gr.split("\n")] for gr in data.split("\n\n")]
 
     print("Part 1")
+    total_yes_any = sum([count_yes_any(ans) for ans in group_answers])
     print("The sum of questions to which anyone answered 'yes'")
-    print(f"for all groups is {count_total(group_answers,'any')}")
+    print(f"for all groups is {total_yes_any}")
     print()
 
     print("Part 2")
+    total_yes_all = sum([count_yes_all(ans) for ans in group_answers])
     print("The sum of questions to which everyone answered 'yes'")
-    print(f"for all groups is {count_total(group_answers,'all')}")
+    print(f"for all groups is {total_yes_all}")
 
 
 if __name__ == "__main__":
