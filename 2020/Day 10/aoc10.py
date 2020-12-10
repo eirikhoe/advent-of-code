a12fc51592a17ab0eb76223c514c5d6b2340d3ea
eirikhoe <eirik150787@hotmail.com>
2020-12-10 12:23:18 +0100

    improved formatting day 10

--- 2020/Day 10/aoc10.py
+++ 2020/Day 10/aoc10.py
@@ -18,31 +18,27 @@ def find_three_one_jolt_diff_prod(joltages):
 
 
 def find_adapter_orderings(joltages):
-
-    groups = []
     jolt_diffs, joltages, n = _find_jolting_diffs(joltages)
     three_jolt_diffs = jolt_diffs == 3
     splits = np.arange(1, n)
     splits = [0, *splits[three_jolt_diffs], n]
 
-    for i, _ in enumerate(splits[:-1]):
-        groups.append(joltages[splits[i] : splits[i + 1]])
-
     amount = 1
-    for group in groups:
-        amount *= do_ordering_rec(group[1:], group[0], group[-1])
+    for i, _ in enumerate(splits[:-1]):
+        group = joltages[splits[i] : splits[i + 1]]
+        amount *= _group_orderings(group[1:], group[0], group[-1])
 
     return amount
 
 
-def do_ordering_rec(joltages, current, end):
+def _group_orderings(joltages, current, end):
     if current == end:
         return 1
     amount = 0
     for i, joltage in enumerate(joltages):
         if joltage - current > 3:
             break
-        amount += do_ordering_rec(joltages[i + 1 :], joltage, end)
+        amount += _group_orderings(joltages[i + 1 :], joltage, end)
     return amount
 
 
