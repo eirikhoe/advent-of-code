42f81e7b61fde07dd65def9cadc11a1b8889ac18
eirikhoe <eirik150787@hotmail.com>
2020-12-12 10:10:11 +0100

    Minor cleanup day 12

--- 2020/Day 12/aoc12.py
+++ 2020/Day 12/aoc12.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 directions = {"E": [0, 1], "W": [0, -1], "N": [-1, 0], "S": [1, 0]}
-headings = ["E", "N", "W", "S"]
 turn_sgn = {"L": 1, "R": -1}
 
 def turn_right(v,deg):
@@ -21,6 +20,7 @@ def find_ferry_distance(instrs):
     instrs = [[instr[0], int(instr[1:])] for instr in instrs]
     heading = [0, 1]
     pos = [0, 0]
+    
     for instr in instrs:
         if instr[0] in ["R", "L"]:
             deg = (instr[1]*turn_sgn[instr[0]]) % 360
@@ -34,6 +34,7 @@ def find_ferry_distance(instrs):
                 raise RuntimeError("Invalid action")
             pos[0] += curr_dir[0] * instr[1]
             pos[1] += curr_dir[1] * instr[1]
+    
     man_dist = abs(pos[0]) + abs(pos[1])
     return man_dist
 
@@ -42,6 +43,7 @@ def find_ferry_distance_with_waypoint(instrs):
     instrs = [[instr[0], int(instr[1:])] for instr in instrs]
     waypoint = [-1, 10]
     pos = [0, 0]
+    
     for instr in instrs:
         if instr[0] in ["R", "L"]:
             deg = (instr[1]*turn_sgn[instr[0]]) % 360
@@ -55,6 +57,7 @@ def find_ferry_distance_with_waypoint(instrs):
             pos[1] += waypoint[1] * instr[1]
         else:
             raise RuntimeError("Invalid action")
+    
     man_dist = abs(pos[0]) + abs(pos[1])
     return man_dist
 
@@ -63,6 +66,7 @@ def main():
     data_folder = Path(".").resolve()
     data = data_folder.joinpath("input.txt").read_text()
     instrs = data.split("\n")
+    
     print("Part 1")
     man_dist = find_ferry_distance(instrs)
     print("The Manhattan distance between the ship's starting and final ")
