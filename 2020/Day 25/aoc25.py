8be0c448ee7dd6b095dce863c53ca6ee852c43b6
eirikhoe <eirik150787@hotmail.com>
2020-12-25 11:42:48 +0100

    Finished 2020, Happy Holidays!

---
+++ 2020/Day 25/aoc25.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+def transformation(value, subject_number):
+    return (value * subject_number) % 20201227
+
+def find_loop_size(public_key):
+    subject_number = 7
+    value = 1
+    loop_size = 0
+    while value != public_key:
+        value = transformation(value,subject_number)        
+        loop_size += 1
+    return loop_size
+
+
+
+
+def main():
+    data_folder = Path(__file__).parent.resolve()
+    data = data_folder.joinpath("input.txt").read_text()
+    card_public_key, door_public_key = (int(d) for d in data.split("\n"))
+    card_loop_size = find_loop_size(card_public_key)
+    encryption_key = 1
+    for i in range(card_loop_size):
+        encryption_key = transformation(encryption_key,door_public_key)
+    
+    print("Part 1")
+    print(f"The encryption key that the door and key is trying ")
+    print(f"to establish is {encryption_key}")
+    print()
+
+
+if __name__ == "__main__":
+    main()
