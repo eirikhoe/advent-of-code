c1f4e57aad663d8ea799a2520ae6a7f0b5069ca0
eirikhoe <eirik.hoeiseth@gmail.com>
2021-12-01 11:26:17 +0100

    Added day 1 2021

--- 2021/Day 01/aoc1.py
+++ 2021/Day 01/aoc1.py
@@ -4,16 +4,32 @@ import numpy as np
 data_folder = Path(".").resolve()
 
 
+def count_increasing_depth(measurements, window_size):
+    measurements = np.array(measurements)
+    n_measurements = measurements.shape[0]
+    windows = np.copy(measurements[: (n_measurements + 1 - window_size)])
+    for i in range(1, window_size):
+        windows += measurements[i : (n_measurements + 1 - window_size + i)]
+    n_increasing = np.sum(np.diff(windows) > 0)
+    return n_increasing
+
+
 def main():
     data = data_folder.joinpath("input.txt").read_text()
     data = [int(d) for d in data.split("\n")]
 
+    measurement_window = 1
+    n_increasing = count_increasing_depth(data, measurement_window)
+
     print("Part 1")
-    print(f"")
+    print(f"{n_increasing} measurements are larger than the previous measurement.")
     print()
 
+    measurement_window = 3
+    n_increasing = count_increasing_depth(data, measurement_window)
+
     print("Part 2")
-    print(f"")
+    print(f"{n_increasing} sums of {measurement_window} are larger than the previous sum.")
     print()
 
 
